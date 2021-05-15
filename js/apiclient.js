5bd722f2e621124b3e20d7e36ae3128790e05045
David Coronado <david.coronado@escuelaing.edu.co>
2021-05-15 16:48:22 -0500

    refactorMenu

--- js/apiclient.js
+++ js/apiclient.js
@@ -1,19 +1,20 @@
 var apiclient = (function () {
-    var client_secret =  'YXBpY2xpZW50OmNsaWVudDEyMw==';
-    var urlApi = "http://localhost:8080/oauth/token";
-    
+    var client_secret = 'YXBpY2xpZW50OmNsaWVudDEyMw==';
+    var urlApi = "http://localhost:8080/";
+
     return {
-        loginUser: function (uname, pword, callback,cberror) {
+        loginUser: function (uname, pword, callback, cberror) {
 
             $.ajax({
-                url: urlApi,
+                url: urlApi + "oauth/token",
                 type: 'POST',
-                headers: {'Authorization': 'Basic '+client_secret, //android-client:android-secret in Base64
-                     "Content-Type" : "application/x-www-form-urlencoded"},
+                headers: {'Authorization': 'Basic ' + client_secret, //android-client:android-secret in Base64
+                    "Content-Type": "application/x-www-form-urlencoded"},
                 data: {username: uname, password: pword, "grant_type": "password"},
                 contentType: "application/x-www-form-urlencoded",
-                'success': function (resp) { 
-                    localStorage.setItem("token",JSON.stringify(resp)); 
+                'success': function (resp) {
+                    localStorage.setItem("token", JSON.stringify(resp)); 
+                    localStorage.setItem("username", uname);
                     callback();
 
                 },
@@ -21,10 +22,25 @@ var apiclient = (function () {
                     cberror(errorThrown);
                 }
             });
+        },
 
+        getDetailsUser: function (callback) {
+            $.ajax({
+                url: urlApi + "ecihorarios/user/" + localStorage.getItem("username"),
+                type: 'GET',
+                headers: {"Authorization": "Bearer " + JSON.parse(localStorage.getItem("token")).access_token},
+                contentType: "application/json",
+                'success': function (resp) {
+                    callback(resp);
+                }
+            });
+        }, 
+        
+        clearSession : function(){
+            localStorage.clear();   
         }
 
-
     };
+
 
 })();
--- js/app.js
+++ js/app.js
@@ -1,31 +1,110 @@
-var app = (function(){  
-    
+var app = (function () {
+
     var dirapi = "js/apiclient.js";
+    var urlApp = "http://localhost/ECIHorarios/";
+    var view = "#Inicio";
+    var detailsUser;
 
-    
-    var checkLoginUser= function(uname,password){
-        $.getScript(dirapi, function() {
-            apiclient.loginUser(uname,password,cbLogin,cbLoginError);
+    var checkLoginUser = function (uname, password) {
+        $.getScript(dirapi, function () {
+            apiclient.loginUser(uname, password, cbLogin, cbLoginError);
         });
-        
+
     };
 
-    var cbLogin = function (){   
-        
-        
-    }; 
-    
-    var cbLoginError = function(resp){ 
+
+    var getDetailsUser = function () {
+        $.getScript(dirapi, function () {
+            apiclient.getDetailsUser(cbDetailsUser);
+        });
+    };
+
+
+    var getAvailableSubject = function () {
+        hideCurrentView();
+        view = "#Preinscribir";
+        showCurrentView();
+
+
+    };
+
+    var getSchedules = function () {
+        hideCurrentView();
+        view = "#Tushorarios";
+        showCurrentView();
+
+    };
+
+    var getMenu = function () {
+        hideCurrentView();
+        view = "#Inicio";
+        showCurrentView();
+
+    };
+
+    var logOut = function () {
+        window.location.href = urlApp + "login.html";
+        $.getScript(dirapi, function () {
+            apiclient.clearSession();
+        });
+
+
+    };
+
+    var cbLogin = function () {
+        getDetailsUser();
+
+    };
+
+    var cbLoginError = function (resp) {
         alert("Username or Password incorrect");
-        
+
     };
 
-    
+    var cbDetailsUser = function (details) {
+        detailsUser = details;
+        window.location.href = urlApp + "menu.html";
+
+
+    };
+
+
+    var hideCurrentView = function () {
+        $(view).hide();
+
+    };
+
+    var showCurrentView = function () {
+        $(view).show();
+    };
+
     return{
-      loginUser:function(username,password){
-          checkLoginUser(username,password); 
-          event.preventDefault();
-      } 
+        loginUser: function (username, password) {
+            checkLoginUser(username, password);
+            event.preventDefault();
+        },
+
+        querySubject: function () {
+            getAvailableSubject();
+            event.preventDefault();
+        },
+
+        getSchedules: function () {
+            getSchedules();
+            event.preventDefault();
+        },
+
+        menu: function () {
+            getMenu();
+            event.preventDefault();
+        },
+
+        exit: function () {
+            logOut();
+            event.preventDefault();
+        }
+
+
     };
 
 
